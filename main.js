70d50f359d61b5d30e91aa08f54a271594dab226
j3ffh95 <j3ffh95@gmail.com>
2021-01-17 11:40:48 -0500

    Added the function openCity to add functionality when hovered

---
+++ main.js
@@ -0,0 +1,13 @@
+function openCity(evt, cityName) {
+  let tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
